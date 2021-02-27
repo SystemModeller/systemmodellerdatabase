ab9f9c663fac155fc165d5582fa7842b298746af
systemmodeller.com <indicatedairspeed@gmail.com>
2021-02-27 21:33:33 +0000

    Revert "Update to stored procedures and names"
    This reverts commit 20182db3db990db51128f60941cdc36439d62aa9.

--- systemmodellerdatabase/Security/RoleMemberships.sql
+++ systemmodellerdatabase/Security/RoleMemberships.sql
@@ -5,7 +5,3 @@ GO
 ALTER ROLE [db_owner] ADD MEMBER [systemmodellerDB]
 GO
 ALTER ROLE [db_securityadmin] ADD MEMBER [systemmodellerDB]
-GO
-ALTER ROLE [db_owner] ADD MEMBER [systemmodellerDB]
-GO
-ALTER ROLE [db_securityadmin] ADD MEMBER [systemmodellerDB]
