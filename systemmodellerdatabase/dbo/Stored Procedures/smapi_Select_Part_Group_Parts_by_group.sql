20182db3db990db51128f60941cdc36439d62aa9
Mark Gibbons <indicatedairspeed@gmail.com>
2021-02-27 21:09:54 +0000

    Update to stored procedures and names

---
+++ systemmodellerdatabase/dbo/Stored Procedures/smapi_Select_Part_Group_Parts_by_group.sql
@@ -0,0 +1,13 @@
+﻿-- =============================================
+-- Author:		Mark Gibbons
+-- Create date: 03/02/2021
+-- Description:	Select devices by Part group ID 
+-- =============================================
+CREATE PROCEDURE [dbo].[smapi_Select_Part_Group_Parts_by_group]
+	@PartGroup_ID int 
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT * FROM Part_Group WHERE Part_Group_ID = @PartGroup_ID
+END
